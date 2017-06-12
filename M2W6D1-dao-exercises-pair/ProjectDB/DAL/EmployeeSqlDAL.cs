2a6dc8edbdaf6ea5dfbe7792177853bba5ec82b6
Josh Mainini <joshmainini@outlook.com>
2017-06-12 15:38:09 -0400

    cleaning up

--- M2W6D1-dao-exercises-pair/ProjectDB/DAL/EmployeeSqlDAL.cs
+++ M2W6D1-dao-exercises-pair/ProjectDB/DAL/EmployeeSqlDAL.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
 namespace ProjectDB.DAL
 {
     public class EmployeeSqlDAL
     {
         private string connectionString;
+		private const string SQL_GetAllEmployees = "SELECT * FROM employee";
 
         // Single Parameter Constructor
         public EmployeeSqlDAL(string dbConnectionString)
@@ -19,8 +20,34 @@ namespace ProjectDB.DAL
 
         public List<Employee> GetAllEmployees()
         {
-            throw new NotImplementedException();
-        }
+			List<Employee> output = new List<Employee>();
+
+			try
+			{
+				using (SqlConnection conn = new SqlConnection(connectionString))
+				{
+					conn.Open();
+
+					SqlCommand cmd = new SqlCommand(SQL_GetAllEmployees, conn);
+					SqlDataReader reader = cmd.ExecuteReader();
+
+					while (reader.Read())
+					{
+						Employee e = new Employee();
+						e.DepartmentId = Convert.ToInt32(reader["department_id"]);
+						e.EmployeeId = Convert.ToInt32(reader["employee_id"]);
+						e.BirthDate = Convert.ToDateTime(reader["birth_date"]);
+
+						output.Add(d);
+					}
+				}
+			}
+			catch (SqlException e)
+			{
+				throw;
+			}
+			return output;
+		}
 
         public List<Employee> Search(string firstname, string lastname)
         {
