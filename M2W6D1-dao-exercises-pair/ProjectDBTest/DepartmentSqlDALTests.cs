cf0f61b6b38e29b8d8b5b65f362500876bf6b598
Josh Mainini <joshmainini@outlook.com>
2017-06-13 13:10:19 -0400

    Updated Exercise

---
+++ M2W6D1-dao-exercises-pair/ProjectDBTest/DepartmentSqlDALTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Data.SqlClient;
+using ProjectDB.Models;
+using System.Collections.Generic;
+using ProjectDB.DAL;
+
+namespace ProjectDBTest
+{
+	[TestClass]
+	public class DepartmentSqlDALTests
+	{
+		private TransactionScope tran;
+		private string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=Projects;Integrated Security=True";
+	
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			tran = new TransactionScope();
+
+		}
+		[TestCleanup]
+		public void Cleanup()
+		{
+			tran.Dispose();
+		}
+
+		[TestMethod]
+		public void GetDepartmentsTest()
+		{
+			DepartmentSqlDAL departments = new DepartmentSqlDAL(connectionString);
+
+			List<Department> department = departments.GetDepartments();
+
+			Assert.IsNotNull(department);
+			Assert.AreEqual(8, department.Count());
+		}
+		[TestMethod]
+		public void CreateDepartmentTest()
+		{
+			DepartmentSqlDAL departments = new DepartmentSqlDAL(connectionString);
+
+			Department ABC = new Department()
+			{
+				Name = "ABC"
+			};
+
+			bool isTrue = departments.CreateDepartment(ABC);
+
+			Assert.AreEqual(true, isTrue);
+		}
+		[TestMethod]
+		public void UpdateDepartment()
+		{
+			DepartmentSqlDAL departments = new DepartmentSqlDAL(connectionString);
+
+			Department ABCD = new Department()
+			{
+				Name = "ABCD",
+				Id = 2
+			};
+
+			bool isTrue = departments.UpdateDepartment(ABCD);
+			List<Department> updatedDepartments = departments.GetDepartments();
+
+			Assert.AreEqual("ABCD", updatedDepartments[0].Name);
+			Assert.AreEqual(true, isTrue);
+
+		}
+	}
+}
---
+++ M2W6D1-dao-exercises-pair/ProjectDBTest/EmployeeSqlDALTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectDB.Models;
+using ProjectDB.DAL;
+using System.Transactions;
+
+
+namespace ProjectDBTest
+{
+	[TestClass]
+	public class EmployeeSqlDALTest
+	{
+		private TransactionScope tran;
+		private string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=Projects;Integrated Security=True";
+
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			tran = new TransactionScope();
+
+		}
+		[TestCleanup]
+		public void Cleanup()
+		{
+			tran.Dispose();
+		}
+
+		[TestMethod]
+		public void GetAllEmployeesTest()
+		{
+			EmployeeSqlDAL employee = new EmployeeSqlDAL(connectionString);
+			List<Employee> employees = employee.GetAllEmployees();
+
+			Assert.AreEqual(12, employees.Count);
+
+			
+		}
+		[TestMethod]
+		public void GetEmployeesWithoutProjectsTest()
+		{
+			EmployeeSqlDAL employee = new EmployeeSqlDAL(connectionString);
+			List<Employee> employees = employee.GetEmployeesWithoutProjects();
+
+			Assert.AreEqual(2, employees.Count);
+		}
+		[TestMethod]
+		public void SearchTest()
+		{
+			EmployeeSqlDAL employee = new EmployeeSqlDAL(connectionString);
+			List<Employee> employees = employee.Search("Flo","Henderson");
+
+			Assert.AreEqual(1, employees.Count);
+		}
+	}
+}
